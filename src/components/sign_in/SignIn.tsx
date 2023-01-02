a26b5e184adbb7c3fdfc7b9bec78acd5f7a79c83
Abai <abai.kuseinov.kg@gmail.com>
2023-01-02 16:59:00 +0600

    abai

---
+++ src/components/sign_in/SignIn.tsx
@@ -0,0 +1,11 @@
+import TextField from '@mui/material/TextField';
+const SignIn = () => {
+  return (
+    <div>
+     <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        
+    </div>
+  )
+}
+
+export default SignIn
